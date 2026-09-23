a63deb5f42ea221164d30b9973414bb7ffd64dea
RJ Trenchard <rjtrenchard@gmail.com>
2026-09-22 20:40:08 -0700

    add people and names

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,28 @@ use bevy::prelude::*;
 
 fn main() {
     App::new()
-    .add_systems(Startup, hello_world)
+    .add_systems(Startup, add_person)
+    .add_systems(Update, (greet_people, hello_world))
     .run();
 }
 
 fn hello_world() {
     println!("Hello world!");
 }
+
+fn add_person(mut commands: Commands) {
+    commands.spawn( (Person, Name("Sir Lancelot".to_string())));
+    commands.spawn( (Person, Name("King Arthur".to_string())));
+}
+
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in &query {
+        println!("hello {}!", name.0);
+    }
+}
+
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
